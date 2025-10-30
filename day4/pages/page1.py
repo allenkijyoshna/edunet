835fa0113feeb0906c93d3772321b73c7b95748a
allenkijyoshna <allenkijyoshna@gmail.com>
2025-10-30 14:33:41 +0530

    Add files via upload

---
+++ day4/pages/page1.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.image("images./image.png", width=200)
+st.title("Welcome to the Student Performance Dashboard")
+st.write("Navigate to the 'Data Visualization' page to explore student performance data through various charts and graphs.")
